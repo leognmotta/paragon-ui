8ce3905c78fa89a5f78f73e04048142c08117f3e
leognmotta <mail@leomotta.me>
2023-04-11 18:04:18 -0300

    feat: add button component

--- packages/components/button/src/button.tsx
+++ packages/components/button/src/button.tsx
@@ -1,5 +1,29 @@
-import { PropsWithChildren } from 'react'
+import React from 'react'
+import { useMergeRefs } from '@paragon-ui/use-merge-refs'
 
-export const Button = ({ children }: PropsWithChildren) => {
-  return <button>{children}</button>
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  isToggleActive?: boolean
 }
+
+export const Button = React.forwardRef<HTMLButtonElement, React.PropsWithChildren<ButtonProps>>(
+  ({ children, ...theirProps }, forwardedRef) => {
+    const ourProps = {
+      'aria-disabled': theirProps.disabled,
+      'aria-pressed': theirProps.disabled ? undefined : theirProps.isToggleActive,
+    } as ButtonProps
+    const buttonRef = React.useRef<HTMLButtonElement>(null)
+
+    return (
+      <button
+        ref={useMergeRefs(buttonRef, forwardedRef)}
+        {...ourProps}
+        {...theirProps}
+        data-element="button"
+      >
+        {children}
+      </button>
+    )
+  },
+)
+
+Button.displayName = 'ParagonButton'
