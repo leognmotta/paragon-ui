a855228449d3491c548f5f2792bef95f403b86f6
leognmotta <mail@leomotta.me>
2023-04-11 18:04:54 -0300

    chore: add stories for button component

--- packages/components/button/src/stories/button.stories.tsx
+++ packages/components/button/src/stories/button.stories.tsx
@@ -1,12 +1,77 @@
-import type { Meta } from '@storybook/react'
+import { useEffect, useState } from 'react'
+import type { Meta, StoryObj } from '@storybook/react'
 
 import { Button } from '../button'
+import type { ButtonProps } from '../button'
 
-const meta: Meta<typeof Button> = {
+import './button.css'
+
+const meta: Meta<ButtonProps> = {
   title: 'Button/test',
+  component: Button,
   decorators: [(story: Function) => <div>{story()}</div>],
 }
 
-export default meta
+const ToggleButton = ({ children, isToggleActive, disabled }: ButtonProps) => {
+  const [toggleActive, setToggleActive] = useState(isToggleActive)
 
-export const WithVariants = () => <Button>Button</Button>
+  useEffect(() => {
+    if (disabled) {
+      setToggleActive(undefined)
+    } else {
+      setToggleActive(isToggleActive)
+    }
+  }, [disabled, isToggleActive])
+
+  const handleClick = () => {
+    setToggleActive(!toggleActive)
+  }
+
+  return (
+    <Button isToggleActive={toggleActive} onClick={handleClick} disabled={disabled}>
+      {children}
+    </Button>
+  )
+}
+
+export const basic: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Button',
+  },
+  argTypes: {
+    disabled: {
+      type: 'boolean',
+    },
+  },
+  render: ({ children, disabled }) => (
+    <Button disabled={disabled} onClick={() => alert(`you've clicked me!`)}>
+      {children}
+    </Button>
+  ),
+}
+
+export const asToggle: StoryObj<ButtonProps> = {
+  args: {
+    children: 'Button',
+  },
+  argTypes: {
+    isToggleActive: {
+      type: 'boolean',
+      description:
+        'If this value is provided, it will provide "aria-pressed" prop so the button behaves as Toggle button',
+      defaultValue: false,
+    },
+    disabled: {
+      type: 'boolean',
+    },
+  },
+  render: ({ children, isToggleActive, disabled }) => {
+    return (
+      <ToggleButton isToggleActive={isToggleActive} disabled={disabled}>
+        {children}
+      </ToggleButton>
+    )
+  },
+}
+
+export default meta
